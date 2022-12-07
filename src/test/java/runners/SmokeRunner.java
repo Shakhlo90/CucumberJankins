88920313767a002c81eb94cdc5aa40a016a35660
Shakhlo <110711642+Shakhlo90@users.noreply.github.com>
2022-12-06 21:58:13 -0500

    commited

--- src/test/java/runners/SmokeRunner.java
+++ src/test/java/runners/SmokeRunner.java
@@ -7,7 +7,12 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "C:\\Users\\tukht\\IdeaProjects\\CucumberBatch14\\src\\test\\resources\\features",
-        glue = "steps"
+        glue = "steps",
+        dryRun = false,
+        tags = "@sprint3",
+        monochrome = true,
+        plugin = {"pretty"}
+
 )
 
 public class SmokeRunner {
